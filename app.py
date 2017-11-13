afb6687361d215d7ac803fc63aa41b3c6b076ee3
allefpablo <allefpablo@gmail.com>
2017-11-13 18:16:19 -0200

    Add logic to buttons and LEDS

--- app.py
+++ app.py
@@ -14,28 +14,43 @@ class Application:
 
     def __init__(self):
         GPIO.setup(self.LED_TEST_BCB_1, GPIO.OUT)
+        GPIO.setup(self.LED_TEST_BCB_2, GPIO.OUT)
+        GPIO.setup(self.LED_TEST_BCB_3, GPIO.OUT)
+        GPIO.setup(self.LED_FAIL, GPIO.OUT)
+        GPIO.setup(self.LED_PASS, GPIO.OUT)
+
+        GPIO.output(self.LED_PASS, GPIO.HIGH)
+        GPIO.output(self.LED_FAIL, GPIO.HIGH)
 
     def run(self):
         while True:
-            GPIO.output(self.LED_TEST_BCB_1, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(self.LED_TEST_BCB_1, GPIO.LOW)
-            time.sleep(0.5)
+            self.scan_keyboard()
 
     def scan_keyboard(self):
-        pass
+        if not GPIO.input(self.BUTTON_TEST_BCB_1):
+            self.start_test_bcb_var_1()
+
+        if not GPIO.input(self.BUTTON_TEST_BCB_2):
+            self.start_test_bcb_var_2()
+
+        if not GPIO.input(self.BUTTON_TEST_BCB_3):
+            self.start_test_bcb_var_3()
 
     def start_test_bcb_var_1(self):
         #TODO: Put here test routin
+        GPIO.output(self.LED_TEST_BCB_1, GPIO.HIGH)
         print("Teste 1 Iniciado")
 
     def start_test_bcb_var_2(self):
         #TODO: Put here test routin
+        GPIO.output(self.LED_TEST_BCB_2, GPIO.HIGH)
         print("Teste 2 Initiado")
 
     def start_test_bcb_var_3(self):
+        GPIO.output(self.LED_TEST_BCB_3, GPIO.HIGH)
         #TODO: Put here test routin
         print("Teste 2 Initiado")
+
 
 """
     Run Application
