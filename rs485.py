2197c9a7324ed987ccdea06fff521769c1077bcd
allefpablo <allefpablo@gmail.com>
2017-11-15 18:35:28 -0200

    Add RS485 test

--- rs485.py
+++ rs485.py
@@ -11,6 +11,15 @@ class Rs485:
         UART.setup("UART5")
 
     def do_rs485_test(self):
+        res = ""
+        test_val = 'B'
         print("Iniciando teste de RS485")
-        # TODO: Envia dado
-        # TODO: Pergunta resposta udc
+        ser = serial.Serial(port = '/dev/tty05', baudrate='115200')
+        ser.open()
+        if ser.isOpen():
+            ser.write(test_val)
+            res = ser.read()
+            ser.close()
+            if res is test_val:
+                return True
+        return False
