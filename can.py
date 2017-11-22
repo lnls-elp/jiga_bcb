5d2f7a5e6bba079cbda9e2f66afc39986b90160f
allefpablo <allefpablo@gmail.com>
2017-11-22 11:25:03 -0200

    Add CAN test

--- can.py
+++ can.py
@@ -1,15 +1,18 @@
-import Adafruit_BBIO.GPIO as GPIO
+import can
 import time
 
 class Can:
 
     def __init__(self):
-        self.setup_pins()
-
-    def setup_pins(self):
-        pass
+        self._bus = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
+        self._id = 0x123
+        self._data = 0xA5
 
     def do_can_test(self):
-        # TODO: Envia dado
-        # TODO: Pergunta resposta udc
-        pass
+        msg = can.Message(arbitration_id = self._id, data = self._data)
+        self._bus.send(msg)
+        print('Mensagem enviada: ' + str(msg))
+        rcv_msg = self._bus.recv()
+        if rcv_msg.data is self._data:
+            return True
+        return False
